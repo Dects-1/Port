781f6710d743848d55446352c1c78f270a56fd50
Dects-1 <114198142+Dects-1@users.noreply.github.com>
2024-03-16 15:38:26 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Imgprofile } from './components/Imgprofile'
+import { Title } from './components/Title'
+
+function App() {
+  const [hide, setHide] = useState(false)
+
+  return (
+    <main id="main">
+      <div className='leftPort'>
+      <div className='imgpro'>
+        <Imgprofile image="/src/assets/react.svg" />
+        </div>
+<div className='name'>
+<Title title="Jetarin Rueangsak (Jay)">
+        <h3>Frontend</h3>
+        </Title>
+</div>
+        
+<Title title='Contact'>
+        <p style={{display: hide? 'none' :'block'}}>
+          Tel : 0948768680
+          <br/>
+          Email : jopprdroppe@gmail.com
+          </p>
+        <button onClick={() => setHide(!hide)}>
+          {hide ?'Show': 'Hide'} </button>
+        
+    </Title>
+
+<Title title='Education'>
+      <p> ปริญญาตรี สาขาเทคโนโลยีสารสนเทศ</p>
+        <p> มหาวิทยาลัยราชภัฏสวนสุนันทา</p>
+
+      </Title>
+      </div>
+
+      <div className='rightPort'>
+        <Title title="Profile">
+        <p>  
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          </p>
+
+        </Title>
+        
+        <Title title="Work Experience">
+        <p>  
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+           </p>
+
+        </Title>
+
+    
+      </div>
+
+
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Imgprofile.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Imgprofile = (props) => {
+
+    return(
+<div className="imgProfile">
+    <img src={props.image}  alt="" />
+ </div>
+    );
+}
